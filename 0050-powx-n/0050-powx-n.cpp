c94761f1efeebd6b229036a7f4a9b6b2a49070dd
ShroukOuda <132494486+ShroukOuda@users.noreply.github.com>
2024-02-12 18:58:32 -0500

    Time: 0 ms (100.00%), Space: 8 MB (20.66%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        // Base case: x^0 is always 1
-        if (n == 0) {
+        if (n == 0) 
             return 1.0;
-        }
-
-        // Recursive case for positive exponent
-        if (n > 0) {
+        if (n > 0) 
+        {
             double halfPow = myPow(x, n / 2);
             return (n % 2 == 0) ? halfPow * halfPow : x * halfPow * halfPow;
-        } else { // Recursive case for negative exponent
+        } 
+        else 
+        { 
             return 1.0 / (x * myPow(x, -(n + 1)));
         }
     }
