dba3079b087e743dfab934075d8ecbdd7b0f306a
ShroukOuda <132494486+ShroukOuda@users.noreply.github.com>
2024-02-20 05:03:49 +0200

    Time: 3 ms (43.60%), Space: 7.5 MB (44.63%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> stk;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+            stk.push(s[i]);
+            else if (s[i] == ')' || s[i] == ']' || s[i] == '}')
+            {
+                if (stk.empty())
+                return false;
+                if ((s[i] == ')' && stk.top() == '(' )||
+                (s[i] == ']' && stk.top() == '[') ||
+                (s[i] == '}' && stk.top() == '{'))
+                {
+                    stk.pop();
+                }
+                else
+                return false;
+            }
+            else
+            return false;
+        }
+       return stk.empty();
+    }
+};
