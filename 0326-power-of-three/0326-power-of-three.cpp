d9283050aaced8f648e8db636bf96f05f1947b66
ShroukOuda <132494486+ShroukOuda@users.noreply.github.com>
2024-02-20 02:56:51 +0200

    Time: 19 ms (35.84%), Space: 8.4 MB (7.77%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if (n <= 0)
+            return false;
+        double res = log10(n) / log10(3);
+        return res == int(res);
+    }
+};
