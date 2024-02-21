6c377ffd63a2ae811cc2689aa3858c7de7e66c96
ShroukOuda <132494486+ShroukOuda@users.noreply.github.com>
2024-02-21 11:50:36 +0200

    Time: 15 ms (83.20%), Space: 21 MB (20.73%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,35 @@
+#include <stack>
+
+class MinStack {
+public:
+    stack<int> stk;
+    int min;
+
+    MinStack() {
+        min = INT_MAX; 
+    }
+
+    void push(int val) {
+        if (val <= min) {
+            stk.push(min);  
+            min = val;   
+        }
+        stk.push(val);
+    }
+
+    void pop() {
+        if (stk.top() == min) {
+            stk.pop();   
+            min = stk.top();
+        }
+        stk.pop();
+    }
+
+    int top() {
+        return stk.top();
+    }
+
+    int getMin() {
+        return min;
+    }
+};
