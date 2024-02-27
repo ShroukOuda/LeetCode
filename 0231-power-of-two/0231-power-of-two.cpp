e5fc3c94c3d0b9779665f754b2e28c9240f8973c
ShroukOuda <132494486+ShroukOuda@users.noreply.github.com>
2024-02-27 13:54:25 +0200

    Time: 0 ms (100.00%), Space: 7.4 MB (6.36%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) 
+    {
+        if (n <= 0)
+            return false;
+        if (log2(n) == int (log2(n)))
+            return true;
+        return false;
+    }
+};
