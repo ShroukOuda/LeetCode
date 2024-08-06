c2ff9b70d514a30d5cf6bf9658c4118c3124ec9d
ShroukOuda <132494486+ShroukOuda@users.noreply.github.com>
2024-08-06 16:30:04 +0300

    Time: 69 ms (75.25%), Space: 23.5 MB (97.67%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,41 +1,23 @@
-#include <iostream>
-#include <map>
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
     int minimumPushes(const std::string &word) {
-        // Step 1: Count frequency of each letter
-        std::map<char, int> frequency;
-        for (char c : word) {
-            frequency[c]++;
-        }
-
-        // Step 2: Transfer frequencies to a vector of pairs
-        std::vector<std::pair<int, char>> freqVector;
-        for (const auto& pair : frequency) {
-            freqVector.push_back({pair.second, pair.first});
-        }
-
-        // Step 3: Sort the vector by frequency in descending order
-        std::sort(freqVector.begin(), freqVector.end(), std::greater<>());
-
-        // Step 4: Calculate minimum pushes
+        vector<int> vec(26);
+        for (auto c : word)
+            vec[c - 'a']++;
+        sort(vec.rbegin(), vec.rend());
         int res = 0;
         int pushes = 1;
         int cnt = 0;
-
-        for (const auto& pair : freqVector) {
-            int freq = pair.first;
-            res += freq * pushes;
-            cnt++;
-            if (cnt == 8) { // Each key can have at most 8 letters
+        for (auto v : vec)
+        {
+            res += v * pushes;
+            cnt ++;
+            if (cnt == 8)
+            {
                 pushes++;
                 cnt = 0;
             }
         }
-
         return res;
     }
 };
