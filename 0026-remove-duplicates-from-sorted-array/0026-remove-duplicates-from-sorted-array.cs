de9cb8d7ca22053ff8029cecb2fe5c37c37057d3
Shrouk Abdelsalam <132494486+ShroukOuda@users.noreply.github.com>
2025-05-19 03:32:39 +0300

    Time: 0 ms (100%), Space: 51.4 MB (11.49%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cs
@@ -1,19 +1,29 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
-        Dictionary<int, int> Dict = new Dictionary<int, int>();
-        foreach (var item in nums)
+        // Dictionary<int, int> Dict = new Dictionary<int, int>();
+        // foreach (var item in nums)
+        // {
+        //     if (Dict.ContainsKey(item))
+        //         Dict[item]++;
+        //     else
+        //         Dict[item] = 1;
+        // }
+        // int i = 0;
+        // foreach (var key in Dict.Keys)
+        // {
+        //     nums[i] = key;
+        //     i++;
+        // }
+        // return Dict.Count;
+        int count = 1;
+        for (int i = 1; i < nums.Length; i++)
         {
-            if (Dict.ContainsKey(item))
-                Dict[item]++;
-            else
-                Dict[item] = 1;
+            if (nums[i] != nums[i - 1])
+            {
+                nums[count] = nums[i];
+                count++;
+            }
         }
-        int i = 0;
-        foreach (var key in Dict.Keys)
-        {
-            nums[i] = key;
-            i++;
-        }
-        return Dict.Count;
+        return count;
     }
 }
